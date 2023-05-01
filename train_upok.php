e27dd51389012abdab221db13e4353d0c4803d6f
jeanjerry <C109154229@nkust.edu.tw>
2023-05-01 22:01:40 +0800

    train_ok

---
+++ train_upok.php
@@ -0,0 +1,41 @@
+<?php
+
+include 'conn.php';
+$time="";
+$account="";
+$action="";
+$degree="";
+$parts="";
+$times="";
+$return["time"]="";
+$return["action"]=false;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$time = trim($_POST['time']);
+$account = trim($_POST['account']);
+$action = trim($_POST['action']);
+$degree = trim($_POST['degree']);
+$parts = trim($_POST['parts']);
+$times = trim($_POST['times']);
+}
+
+
+$sql2 = $conn->query("select * FROM `action` where account = '$account'AND action = '$action'AND time = '$time'");
+
+if ($sql2->num_rows > 0){
+    $conn->query("update action set times='2' where account ='$account'AND action = '$action'AND time = '$time'");
+    $conn->query("update trainup set times='2' where account ='$account'AND action = '$action'AND time = '$time'");
+}
+else{
+    $sql =$conn->query("insert into action(times,time,degree,parts,action,account) 
+    values('".$times."','".$time."','".$degree."','".$parts."','".$action."','".$account."')");
+    $sql1 =$conn->query("insert into trainup(times,time,degree,parts,action,account) 
+    values('".$times."','".$time."','".$degree."','".$parts."','".$action."','".$account."')");
+}
+
+
+
+
+  
+  
+
+?>
