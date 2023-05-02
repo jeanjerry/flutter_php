da67e7d73994be0724ce4d4aad8a05c39c4d7f04
jeanjerry <C109154229@nkust.edu.tw>
2023-05-03 01:24:48 +0800

    trainok

--- train_downok.php
+++ train_downok.php
@@ -7,8 +7,8 @@ $action="";
 $degree="";
 $parts="";
 $times="";
-$return["time"]="";
-$return["action"]=false;
+$coin_add="";
+$coin=0;
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 $time = trim($_POST['time']);
 $account = trim($_POST['account']);
@@ -16,11 +16,11 @@ $action = trim($_POST['action']);
 $degree = trim($_POST['degree']);
 $parts = trim($_POST['parts']);
 $times = trim($_POST['times']);
+$coin_add = trim($_POST['coin_add']);
 }
 
 
-$sql2 = $conn->query("select * FROM `action` where account = '$account'AND action = '$action'AND time = '$time'");
-
+$sql2 = $conn->query("select * FROM `action` where account = '$account'");
 if ($sql2->num_rows > 0){
     $conn->query("update action set times='2' where account ='$account'AND action = '$action'AND time = '$time'");
     $conn->query("update traindown set times='2' where account ='$account'AND action = '$action'AND time = '$time'");
@@ -30,11 +30,21 @@ else{
     values('".$times."','".$time."','".$degree."','".$parts."','".$action."','".$account."')");
     $sql1 =$conn->query("insert into traindown(times,time,degree,parts,action,account) 
     values('".$times."','".$time."','".$degree."','".$parts."','".$action."','".$account."')");
+    
 }
 
+$sql3 = $conn->query("select coin FROM `co` where account = '$account'");
 
-
-
+if ($sql3->num_rows > 0) {
+    $row = mysqli_fetch_assoc($sql3) ;
+            // 每跑一次迴圈就抓一筆值，最後放進data陣列中
+            $coin = $row['coin'];
+            echo"$coin";        
+        (int)$coin=(int)$coin+(int)$coin_add;
+        
+    $conn->query("update co set coin= $coin where account ='$account'");
+   
+    }
   
   
 
--- train_upok.php
+++ train_upok.php
@@ -8,8 +8,6 @@ $degree="";
 $parts="";
 $times="";
 $coin_add="";
-$return["time"]="";
-$return["action"]=false;
 $coin=0;
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 $time = trim($_POST['time']);
@@ -22,7 +20,7 @@ $coin_add = trim($_POST['coin_add']);
 }
 
 
-$sql2 = $conn->query("select * FROM `action` where account = '$account'AND action = '$action'AND time = '$time'");
+$sql2 = $conn->query("select * FROM `action` where account = '$account'");
 if ($sql2->num_rows > 0){
     $conn->query("update action set times='2' where account ='$account'AND action = '$action'AND time = '$time'");
     $conn->query("update trainup set times='2' where account ='$account'AND action = '$action'AND time = '$time'");
@@ -32,19 +30,20 @@ else{
     values('".$times."','".$time."','".$degree."','".$parts."','".$action."','".$account."')");
     $sql1 =$conn->query("insert into trainup(times,time,degree,parts,action,account) 
     values('".$times."','".$time."','".$degree."','".$parts."','".$action."','".$account."')");
+    
 }
 
-$sql3 = $conn->query("select coin FROM `co` where account = 'airehab_01'");
-echo "ok";
+$sql3 = $conn->query("select coin FROM `co` where account = '$account'");
+
 if ($sql3->num_rows > 0) {
-    while ($row = mysqli_fetch_assoc($sql3)) {
+    $row = mysqli_fetch_assoc($sql3) ;
             // 每跑一次迴圈就抓一筆值，最後放進data陣列中
-            $coin = $row;
-            echo "ook";
-        }
+            $coin = $row['coin'];
+            echo"$coin";        
         (int)$coin=(int)$coin+(int)$coin_add;
-    $conn->query("update co set coin= $coin where account ='airehab_01'");
-    echo "oook";
+        
+    $conn->query("update co set coin= $coin where account ='$account'");
+   
     }
   
   
